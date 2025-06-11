dfd51e0a805bcbc0541fee244b652c7f0de31900
qminhminh <hqminh050503@gmail.com>
2025-06-11 11:26:37 +0700

    Refactor package structure to 'com.nedok.NedokCamera.NedokCamera' across Android and iOS configurations, update application ID, and remove unused logo assets.

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,7 +1,7 @@
 abstract class ThingsboardAppConstants {
   static const thingsBoardApiEndpoint = 'https://datahub-dev.i-nedok.com';
   static const thingsboardOAuth2CallbackUrlScheme =
-      'com.nedok.NedokCamera.auth';
+      'com.nedok.NedokCamera.NedokCamera.auth';
 
   /// Not for production (only for debugging)
   static const thingsboardOAuth2AppSecret = 'Your app secret here';
